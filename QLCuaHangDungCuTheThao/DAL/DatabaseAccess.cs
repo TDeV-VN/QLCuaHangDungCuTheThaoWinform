e046706dbcfeb6df116ceee0feafaf03abb001b0
Tien Nguyen <52200088@student.tdtu.edu.vn>
2024-03-26 09:17:15 +0700

    Khoi tao

---
+++ QLCuaHangDungCuTheThao/DAL/DatabaseAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DatabaseAccess
+    {
+        // Tạo kết nối đến cơ sở dữ liệu
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = @"";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
